9fc2232e37e73c8726806ccd7b90f50c96b91e21
tsinghqy <tsinghqy@gmail.com>
2024-04-13 12:12:51 -0400

    Updated site

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+// Example: Change background color on button click
+document.getElementById("changeColorBtn").addEventListener("click", function() {
+    document.body.style.backgroundColor = "lightblue";
+});
